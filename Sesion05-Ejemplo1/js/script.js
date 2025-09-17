82e68d62885531e99f0f2a71954d982524b8e234
JCTOVARU <jctovarueda@gmail.com>
2025-09-16 21:34:26 -0500

    Desarrollo (Avance 2) de la Sesion N° 5: CSS Microinteracciones

--- Sesion05-Ejemplo1/js/script.js
+++ Sesion05-Ejemplo1/js/script.js
@@ -1,1 +1,11 @@
 //Javascript
+function procesarInfo() {
+  const loader = document.getElementById('loader');
+  loader.style.display = 'inline-block';
+  // Simula un proceso que tarda 2 segundos, como si simulara el envío de un formulario
+  // al backend
+  setTimeout(() => {
+    loader.style.display = 'none';
+    alert('Información enviada con éxito');
+  }, 2000);
+}
